ff884edde1976a4058914e88b1237ce8104e4cbb
QuasiStellar <quasistellar.official@gmail.com>
2022-12-28 01:18:40 +0300

    Make home page stateless

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,14 +6,9 @@ import 'package:website/utils/outlined_text.dart';
 
 import 'content.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
